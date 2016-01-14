7c385f1f65005b4b2f6c78c235ee35a1ec8f78f3
JGaudion <JGaudion@scottlogic.co.uk>
2016-01-14 14:08:50 +0000

    Just comments

--- OpenIDConnect.Host.AspNet/EntitiesAndStores/RoleStore.cs
+++ OpenIDConnect.Host.AspNet/EntitiesAndStores/RoleStore.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 
 namespace OpenIDConnect.Host.AspNet.EntitiesAndStores
 {
+    /// <summary>
+    /// This is my implementation of the RoleStore. It is needed for the Application Role Manager
+    /// </summary>
     public class RoleStore : RoleStore<Role>
     {
-        public RoleStore(Context ctx) :base (ctx)
+        /// <summary>
+        /// The base class needs to know the context (that communicates with the data). 
+        /// The Context is an implementation of IdentityDbContext<MyUserImplementation> 
+        /// </summary>
+        /// <param name="ctx"></param>
+        public RoleStore(NameMattersContext ctx) :base (ctx)
         {
 
         }
--- OpenIDConnect.Host.AspNet/IdSvr/ExtensionMethods.cs
+++ OpenIDConnect.Host.AspNet/IdSvr/ExtensionMethods.cs
@@ -15,11 +15,17 @@ namespace OpenIDConnect.Host.AspNet.IdSvr
     {
         public static void ConfigureSimpleIdentityManagerService(this IdentityManagerServiceFactory factory, string connectionString)
         {
-            factory.Register(new IdentityManager.Configuration.Registration<Context>(resolver => new Context(connectionString)));
+            ///This is telling the IdentityManagerServiceFactory which dependencies it needs (using my implementations)
+            factory.Register(new IdentityManager.Configuration.Registration<NameMattersContext>(resolver => new NameMattersContext(connectionString)));
             factory.Register(new Registration<UserStore>());
             factory.Register(new Registration<RoleStore>());
             factory.Register(new Registration<UserManager>());
             factory.Register(new Registration<RoleManager>());
+            ///Register which identity manager service we are using. The second part (Simple Identity Manager Service) points to our
+            ///actual implementiation. The IIdentityManagerService contains default async methods for things like adding a user
+            ///or getting the user claims etc
+            ///This style of registration means: the first thing is what is dependant, the second is what it is dependant on (the implementation to use)
+            ///e.g. SimpleIdentityManagerService is the concrete version of IIdentityManagerService
             factory.IdentityManagerService = new Registration<IIdentityManagerService, SimpleIdentityManagerService>();
         }
 
@@ -28,7 +34,7 @@ namespace OpenIDConnect.Host.AspNet.IdSvr
             factory.UserService = new IdentityServer3.Core.Configuration.Registration<IdentityServer3.Core.Services.IUserService, UserService>();
             factory.Register(new IdentityServer3.Core.Configuration.Registration<UserManager>());
             factory.Register(new IdentityServer3.Core.Configuration.Registration<UserStore>());
-            factory.Register(new IdentityServer3.Core.Configuration.Registration<Context>(resolver => new Context(connString)));
+            factory.Register(new IdentityServer3.Core.Configuration.Registration<NameMattersContext>(resolver => new NameMattersContext(connString)));
         }
     }
 }
