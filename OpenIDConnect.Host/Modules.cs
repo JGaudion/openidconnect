6b856009300e4113f205b9bbb2ff73c717f73356
Michael Tinning <m.t.tinning@gmail.com>
2016-01-15 14:09:57 +0000

    Adding host module registration

--- OpenIDConnect.Host/Modules.cs
+++ OpenIDConnect.Host/Modules.cs
@@ -2,7 +2,10 @@
 using OpenIDConnect.AdLds.Factories;
 using OpenIDConnect.AdLds.Models;
 using OpenIDConnect.AdLds.Services;
+using OpenIDConnect.Core;
 using OpenIDConnect.Core.Services;
+using OpenIDConnect.IdentityServer.Factories;
+using System;
 
 namespace OpenIDConnect.Host
 {
@@ -10,12 +13,22 @@ namespace OpenIDConnect.Host
     {
         public static void RegisterAll(ContainerBuilder builder)
         {
+            RegisterHost(builder);
             RegisterAdLds(builder);
+        }
+
+        public static void RegisterHost(ContainerBuilder builder)
+        {
+            var configService = new ApplicationSettingsConfigurationService();
+            builder.Register(ctx => configService).As<IConfigurationService>();
+            UserStoreType userStoreType;
+            Enum.TryParse(configService.GetSetting("IdentityServerUserStore", "None"), out userStoreType);
+            builder.Register(ctx => userStoreType);
         }
 
         public static void RegisterAdLds(ContainerBuilder builder)
         {
-            builder.RegisterType<AdLdsUserService>().As<IUserAuthenticationService>();
+            builder.RegisterType<AdLdsUserService>();
             builder.RegisterType<AdLdsDirectoryContextFactory>().As<IDirectoryContextFactory>();
             builder.Register(ctx => new DirectoryConnectionConfig("localhost", "389", "LDAP://", "CN=ADLDSUsers,DC=ScottLogic,DC=local"));
         }
