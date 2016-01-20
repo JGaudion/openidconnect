a72bcd743f64bf9c46fbd9200e3c8c1181385557
John Dawes <jdawes@scottlogic.co.uk>
2016-01-20 17:14:14 +0000

    Add TypeUtilities class to set properties
    Add TypeUtilities class to encapsulate setting arbitrary properties on
    account objects.

---
+++ OpenIDConnect.Core/Utilities/TypeUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using OpenIDConnect.Core.Models.UserManagement;
+
+namespace OpenIDConnect.Core.Utilities
+{
+    public static class TypeUtilities
+    {
+        public static UserManagementResult SetProperty(object instance, string propertyName, string value)
+        {
+            try
+            {
+                Type type = instance.GetType();
+                PropertyInfo prop = type.GetProperty(propertyName, BindingFlags.Public);
+                
+                Type conversionType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+
+                object obj;
+                try
+                {
+                    obj = Convert.ChangeType(value, conversionType);
+                }
+                catch
+                {
+                    return new UserManagementResult(new[] { $"Conversion of {value} to {conversionType.FullName} failed." });
+                }
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    prop.SetValue(instance, null);
+                }
+                else
+                {
+                    prop.SetValue(instance, obj);
+                }
+
+                return UserManagementResult.Success;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error setting property type " + propertyName, e);
+            }
+        }
+
+    }
+}
