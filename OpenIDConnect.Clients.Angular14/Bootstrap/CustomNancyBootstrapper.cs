3294a6fc2f8198c9e7215228a0aab7c30dab774f
ilovell <ilovell@scottlogic.co.uk>
2016-01-21 11:44:05 +0000

    Added Angular 1.4 client project

---
+++ OpenIDConnect.Clients.Angular14/Bootstrap/CustomNancyBootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy;
+using Nancy.Conventions;
+using System.IO;
+
+namespace OpenIDConnect.Clients.Angular14.Bootstrap
+{
+    public class CustomNancyBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override IRootPathProvider RootPathProvider
+        {
+            get { return new CustomRootPathProvider(new DefaultRootPathProvider()); }
+        }
+    }
+
+    internal class CustomRootPathProvider : IRootPathProvider
+    {
+        private readonly IRootPathProvider provider;
+
+        public CustomRootPathProvider(IRootPathProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public string GetRootPath()
+        {
+            var path = Path.Combine(this.provider.GetRootPath(), "content/dist/");
+            return path;
+        }
+    }   
+}
---
+++ OpenIDConnect.Clients.Angular14/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Extensions;
+
+[assembly: OwinStartup(typeof(OpenIDConnect.Clients.Angular14.Startup))]
+
+namespace OpenIDConnect.Clients.Angular14
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
