84b5d338d51edbb2eda6017d5ac0e639d6cf9038
ilovell <ilovell@scottlogic.co.uk>
2016-01-21 17:34:13 +0000

    Added sign out button

--- OpenIDConnect.Clients.Angular14/content/src/app/components/navbar/navbar.directive.js
+++ OpenIDConnect.Clients.Angular14/content/src/app/components/navbar/navbar.directive.js
@@ -1,37 +1,38 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('src')
-    .directive('acmeNavbar', acmeNavbar);
-
-  /** @ngInject */
-  function acmeNavbar() {
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html',
-      scope: {
-      },
-      controller: NavbarController,
-      controllerAs: 'vm',
-      bindToController: true
-    };
-
-    return directive;
+    angular
+      .module('src')
+      .directive('acmeNavbar', acmeNavbar);
 
     /** @ngInject */
-    function NavbarController(moment, $log, oidc) {
-      var vm = this;
+    function acmeNavbar() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/components/navbar/navbar.html',
+            scope: {
+            },
+            controller: NavbarController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
 
-      vm.signIn = function () {
-          debugger;
-          var mgr = oidc.get();
+        return directive;
 
-          if (mgr.expired) {
-              mgr.redirectForToken();
-          }
-      }
+        /** @ngInject */
+        function NavbarController(moment, $log, oidc) {
+            var vm = this;
+            vm.mgr = oidc.get();
+
+            vm.signIn = function () {
+                if (vm.mgr.expired) {
+                    vm.mgr.redirectForToken();
+                }
+            };
+
+            vm.signOut = function () {
+                vm.mgr.redirectForLogout();
+            };
+        }
     }
-  }
-
 })();
