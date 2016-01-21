f728c1e6dd03bb9d4c73f5b7558609b9e284ed81
ilovell <ilovell@scottlogic.co.uk>
2016-01-21 12:32:37 +0000

    Added sample news controller for Angular client app

---
+++ OpenIDConnect.Clients.Angular14/Models/NewsArticleApiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenIDConnect.Clients.Angular14.Models
+{
+    public class NewsArticleApiModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
--- OpenIDConnect.Clients.Angular14/Startup.cs
+++ OpenIDConnect.Clients.Angular14/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using Microsoft.Owin.Extensions;
+using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
 
 [assembly: OwinStartup(typeof(OpenIDConnect.Clients.Angular14.Startup))]
 
@@ -10,6 +14,23 @@ namespace OpenIDConnect.Clients.Angular14
     {
         public void Configuration(IAppBuilder app)
         {
+            var configuration = new HttpConfiguration();
+
+            // Setup camelCase JSON serialization
+            var jsonFormatter =
+                configuration.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
+
+            if (jsonFormatter != null)
+            {
+                jsonFormatter.SerializerSettings.ContractResolver
+                    = new CamelCasePropertyNamesContractResolver();
+            }
+
+            // Set up attribute based routing
+            configuration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(configuration);
+
             app.UseNancy();
             app.UseStageMarker(PipelineStage.MapHandler);
         }
