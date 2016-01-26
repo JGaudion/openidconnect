03e59212305f01cc543c67da215a95bc20b16646
ilovell <ilovell@scottlogic.co.uk>
2016-01-26 15:43:24 +0000

    Added Authorization API projects

---
+++ OpenIDConnect.Authorization.Data.EntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenIDConnect.Authorization.Data.EntityFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Scott Logic Ltd.")]
+[assembly: AssemblyProduct("OpenIDConnect.Authorization.Data.EntityFramework")]
+[assembly: AssemblyCopyright("Copyright © Scott Logic Ltd. 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bfe3b46-7c35-4492-a549-f1e848316472")]
