42b19d526c6eb36c0b1f23907786365289b101c6
JGaudion <JGaudion@scottlogic.co.uk>
2016-01-26 17:01:33 +0000

    Initial client app attempt

---
+++ OpenIDConnect.Clients.AngularMaterials/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OpenIDConnect.Clients.AngularMaterials.Startup))]
+
+namespace OpenIDConnect.Clients.AngularMaterials
+{
+    /// <summary>
+    /// This is an "Owin Startup Class" (ie. Add > New Items> Web> Owin Startup)
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Use nancy
+            app.UseNancy();
+                        
+        }
+    }
+}
