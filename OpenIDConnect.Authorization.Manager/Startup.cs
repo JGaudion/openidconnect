0015a3ffd483e1dacf7e515106508c6874ad911b
Michael Tinning <m.t.tinning@gmail.com>
2016-01-28 08:59:36 +0000

    Changing name authentication -> authorization

--- OpenIDConnect.Authorization.Manager/Startup.cs
+++ OpenIDConnect.Authorization.Manager/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace OpenIDConnect.Authentication.Manager
+namespace OpenIDConnect.Authorization.Manager
 {
     public class Startup
     {
