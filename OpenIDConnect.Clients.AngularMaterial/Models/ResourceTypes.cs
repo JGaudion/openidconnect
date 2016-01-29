6ef6abe3988430d938d271b26bac3ce0e617666a
John Dawes <jdawes@scottlogic.co.uk>
2016-01-29 12:07:34 +0000

    Implement ResourceAuthorize

---
+++ OpenIDConnect.Clients.AngularMaterial/Models/ResourceTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenIDConnect.Clients.AngularMaterial.Models
+{
+    public class ResourceTypes
+    {
+        public const string Cartoons = "cartoons";
+
+        public const string Characters = "characters";
+
+        public const string Information = "information";
+    }
+}
