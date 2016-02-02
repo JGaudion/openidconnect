bb2cf075463f1f8dbed58356acb417d0b4abc246
JGaudion <JGaudion@scottlogic.co.uk>
2016-02-02 16:41:48 +0000

    Razor custom view service

---
+++ OpenIDConnect.Clients.AngularMaterial/content/scripts/CartoonController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+angular.module('CartoonsApp')
+.controller('CartoonController', CartoonController);
+
+    CartoonController.$inject = ['$http', 'CartoonsService', 'CharacterService'];
+    function CartoonController($http, CartoonsService, CharacterService) {
+    var ctrl = this;
+
+        CartoonsService.get().then(function (data) {
+            ctrl.cartoons = data;
+        });
+
+        CharacterService.get().then(function (data) {
+            ctrl.characters = data;
+        });
+    };
+})();
---
+++ OpenIDConnect.Clients.AngularMaterial/content/scripts/CharacterService.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('CartoonsApp')
+        .factory('CharacterService', CharacterService);
+
+    CharacterService.$inject = ['$http', 'OidcService'];
+
+    function CharacterService($http, OidcService) {
+        var self = this;
+
+        self.get = function () {
+            return $http(
+                {
+                    method: 'GET',
+                    url: '/api/characters',
+                    headers: { 'Authorization': 'Bearer ' + OidcService.tokenManager.access_token }
+                }).then(function (response) {
+                    return response.data;
+                });
+        }
+
+        return self;
+    };
+
+})();
