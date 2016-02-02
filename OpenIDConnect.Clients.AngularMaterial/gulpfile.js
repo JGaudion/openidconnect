45e43304a527e0376c43539be25efc5d63f9b924
John Dawes <jdawes@scottlogic.co.uk>
2016-02-02 09:35:53 +0000

    Refactor sass task in gulpfile

--- OpenIDConnect.Clients.AngularMaterial/gulpfile.js
+++ OpenIDConnect.Clients.AngularMaterial/gulpfile.js
@@ -1,16 +1,17 @@
-﻿var gulp = require('gulp');
+'use strict';
+
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyJs = require('gulp-uglify');
 var useref = require('gulp-useref');
-var minifyCss = require('gulp-cssnano');
+var cssnano = require('gulp-cssnano');
 var del = require('del');
 var sequence = require('run-sequence');
 var gulpIf = require('gulp-if');
 
 var outputFolder = 'dist';
 var contentFolder = 'content';
-var sassFiles = contentFolder + '/scss/*.scss';
-var cssBuildFolder = contentFolder + '/css';
+var sassFiles = contentFolder + '/**/*.scss';
 var cssOutputFolder = outputFolder + '/css';
 var htmlFiles = contentFolder + '/**/*.html';
 
@@ -18,17 +19,21 @@ gulp.task('clean', function () {
     return del([outputFolder, cssBuildFolder]);
 });
 
-gulp.task('compileSass', function () {
-    return gulp.src(sassFiles)
-        .pipe(sass())
-        .pipe(gulp.dest(cssBuildFolder));
+gulp.task('sass', function(){
+    gulp.src(sassFiles)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(gulp.dest(cssOutputFolder));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(sassFiles, ['sass']);
 });
 
 gulp.task('buildPage', function () {
     return gulp.src(htmlFiles)
         .pipe(useref())
         .pipe(gulpIf('*.js', minifyJs({ mangle: false })))
-        .pipe(gulpIf('*.css', minifyCss()))
         .pipe(gulp.dest(outputFolder));
 });
 
