8937dcc072912d50da556ed03b15ef93f303299b
JGaudion <JGaudion@scottlogic.co.uk>
2016-02-02 16:41:48 +0000

    Razor custom view service

--- OpenIDConnect.Clients.AngularMaterial/content/scripts/CartoonController.js
+++ OpenIDConnect.Clients.AngularMaterial/content/scripts/CartoonController.js
@@ -4,12 +4,16 @@
 angular.module('CartoonsApp')
 .controller('CartoonController', CartoonController);
 
-    CartoonController.$inject = ['$http', 'CartoonsService'];
-    function CartoonController($http, CartoonsService) {
+    CartoonController.$inject = ['$http', 'CartoonsService', 'CharacterService'];
+    function CartoonController($http, CartoonsService, CharacterService) {
     var ctrl = this;
 
         CartoonsService.get().then(function (data) {
             ctrl.cartoons = data;
+        });
+
+        CharacterService.get().then(function (data) {
+            ctrl.characters = data;
         });
     };
 })();
--- OpenIDConnect.IdentityServer/Services/CustomViewService.cs
+++ OpenIDConnect.IdentityServer/Services/CustomViewService.cs
@@ -9,6 +9,8 @@ using IdentityServer3.Core.Validation;
 using IdentityServer3.Core.ViewModels;
 using System.IO;
 using System.ComponentModel;
+using RazorEngine;
+using RazorEngine.Templating;
 
 namespace OpenIDConnect.IdentityServer.Services
 {
@@ -22,7 +24,7 @@ namespace OpenIDConnect.IdentityServer.Services
 
         public Task<Stream> ClientPermissions(ClientPermissionsViewModel model)
         {
-            return Render(model, "Permissions");
+            throw new NotImplementedException();
         }
 
         public Task<Stream> Consent(ConsentViewModel model, ValidatedAuthorizeRequest authorizeRequest)
@@ -61,18 +63,15 @@ namespace OpenIDConnect.IdentityServer.Services
         #region Rendering the custom html
         protected virtual Task<System.IO.Stream> Render(CommonViewModel model, string page, string clientName = null)
         {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(model, Newtonsoft.Json.Formatting.None, new Newtonsoft.Json.JsonSerializerSettings() { ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver() });
-
-            string html = LoadHtml(page);
-            html = Replace(html, new
-            {
-                siteName = SoftwareKobo.Net.WebUtility.HtmlEncode(model.SiteName),
-                model = SoftwareKobo.Net.WebUtility.HtmlEncode(json),
-                clientName = clientName
-            });
-
+            //Just hardcoded for now
+            bool usingRazor = true;
+            
+            //swap between razor and html
+            string html = usingRazor ? LoadRazor(page, model) : LoadHtml(page, model, clientName);
             return Task.FromResult(StringToStream(html));
         }
+
+        //HTML only
         string Replace(string value, IDictionary<string, object> values)
         {
             foreach (var key in values.Keys)
@@ -87,12 +86,46 @@ namespace OpenIDConnect.IdentityServer.Services
             return value;
         }
 
-        private string LoadHtml(string name)
+        /// <summary>
+        /// Normal load html for flat html files
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private string LoadHtml(string name, CommonViewModel model, string clientName = null)
         {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(model, Newtonsoft.Json.Formatting.None, new Newtonsoft.Json.JsonSerializerSettings() { ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver() });
+
             //Hardcoded for now
             var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"CustomView");
             file = Path.Combine(file, name + ".html");
-            return File.ReadAllText(file);
+            var html = File.ReadAllText(file);
+
+            html = Replace(html, new
+            {
+                siteName = SoftwareKobo.Net.WebUtility.HtmlEncode(model.SiteName),
+                model = SoftwareKobo.Net.WebUtility.HtmlEncode(json),
+                clientName = clientName
+            });
+
+            return html;
+        }
+
+        /// <summary>
+        /// Compiling the razor files first, then converting them to a flat html string
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private string LoadRazor(string name, CommonViewModel model)
+        {
+            //Hardcoded for now
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"RazorCustomView");
+            file = Path.Combine(file, name + ".cshtml");
+            var htmlContent = File.ReadAllText(file);
+
+            var uniqueKey = Guid.NewGuid(); //I think the template keys are meant to be unique
+            string razorContent = Engine.Razor.RunCompile(htmlContent, uniqueKey.ToString(), null, model);
+
+            return razorContent;
         }
 
         string Replace(string value, object values)
@@ -100,6 +133,7 @@ namespace OpenIDConnect.IdentityServer.Services
             return Replace(value, Map(values));
         }
 
+        //HTML only
         IDictionary<string, object> Map(object values)
         {
             var dictionary = values as IDictionary<string, object>;
