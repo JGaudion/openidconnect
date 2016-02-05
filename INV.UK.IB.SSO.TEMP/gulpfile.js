de8e46348e28c23975c38cc3efed71216365b947
JGaudion <JGaudion@scottlogic.co.uk>
2016-02-05 12:05:13 +0000

    Added a temporary project, while licence issues are occuring

---
+++ INV.UK.IB.SSO.TEMP/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp');
+var raml2html = require('gulp-raml2html');
+var outputFolder = 'Dist';
+
+gulp.task('generateAPI', function () {
+    return gulp.src('./Documentation/UsersandAccessManagementAPI.raml')
+        .pipe(raml2html())
+        .pipe(gulp.dest(outputFolder))
+
+})
